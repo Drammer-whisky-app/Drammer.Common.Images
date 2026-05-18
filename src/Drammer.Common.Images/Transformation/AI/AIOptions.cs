148787921479295897e55504fa2a609b269894f8
Marthijn van den Heuvel <1124723+marthijn@users.noreply.github.com>
2026-05-18 15:17:32 +0200

    Removed all ImageSharp related code due to required licenses for open source projects

--- src/Drammer.Common.Images/Transformation/AI/AIOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Drammer.Common.Images.Transformation.AI;
-
-public sealed class AIOptions
-{
-    public string Endpoint { get; set; }
-
-    public string Key { get; set; }
-}
--- src/Drammer.Common.Images/Transformation/IWaterMarkService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Drammer.Common.Images.Transformation;
-
-public interface IWaterMarkService
-{
-    Task<byte[]> AddWaterMarkAsync(
-        byte[] image,
-        byte[] waterMark,
-        CancellationToken cancellationToken = default);
-}
